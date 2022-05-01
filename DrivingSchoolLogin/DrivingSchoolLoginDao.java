1131558b60c27f1c7d17389fdfd5470b24692e11
Metpalli <99045354+Metpalli@users.noreply.github.com>
2022-05-01 18:29:07 -0400

    Add files via upload
    driving school login

---
+++ DrivingSchoolLogin/DrivingSchoolLoginDao.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.gi.login.DrivingSchoolLogin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author DELL
+ */
+public class DrivingSchoolLoginDao {
+    Connection con;
+    
+    public DrivingSchoolLoginDao() throws ClassNotFoundException, SQLException {
+        //load driver
+        String driver = "com.mysql.cj.jdbc.Driver";
+        Class.forName(driver);
+
+        //create  connection
+        //url : protocol + IP + port_number + db_name
+        String url = "jdbc:mysql://localhost:3306/driving";
+        String user = "root";
+        String password = "";
+        con = DriverManager.getConnection(url, user, password);
+    }
+    public int updatePassword(int id,String password)throws Exception
+    {
+        String query = "update driving_school set password = ? where driving_school_id = ?";
+        PreparedStatement st = con.prepareStatement(query);
+        st.setString(1, password);
+        st.setInt(2,id);
+        return st.executeUpdate();
+    }
+    public DrivingSchoolLoginModel fetchDrivingSchool(DrivingSchoolLoginModel lm) throws SQLException {
+        //int id = cm.getId();
+        String mobile = lm.getMobile();
+        String password = lm.getPassword();
+
+        String query = "select * from driving_school where mobile=? and password=?";
+
+        PreparedStatement st = con.prepareStatement(query);
+        st.setString(1, mobile);
+        st.setString(2, password);
+        ResultSet rst;
+        rst = st.executeQuery();
+        
+        if(rst.next())
+        {
+            DrivingSchoolLoginModel  obj = new DrivingSchoolLoginModel();
+            obj.setDid(rst.getInt("driving_school_id"));
+            obj.setDname(rst.getString("name"));
+            return obj;
+            
+        }
+        else
+        {
+            return null;
+        }
+
+    }
+}
