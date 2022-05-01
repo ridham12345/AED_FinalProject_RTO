9ea4a06de553fffa12a9d63db127de04e9340619
Metpalli <99045354+Metpalli@users.noreply.github.com>
2022-05-01 18:27:19 -0400

    Add files via upload
    driving school

---
+++ drivingschool/ApplicantDetails.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.drivingschool;
+
+import DrivingSchoolHome.ApplicantModel;
+
+/**
+ *
+ * @author acer
+ */
+public class ApplicantDetails {
+    ApplicantModel applicant;
+    int id;
+
+    public ApplicantModel getApplicant() {
+        return applicant;
+    }
+
+    public void setApplicant(ApplicantModel applicant) {
+        this.applicant = applicant;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
